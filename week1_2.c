14b990ba2651844775c4fd288332349d4d931fd3
Pratyasha Kar <65580309+Pratyasha2000@users.noreply.github.com>
2021-02-07 23:23:03 +0530

    Correction

--- week1_2.c
+++ week1_2.c
@@ -3,7 +3,7 @@ int main()
 {
         double  f,c;
 	printf("Enter the temperature in degree Celsius:");
-	scanf("%lf",c);
+	scanf("%lf",&c);
 	f=(1.8*c)+32;
 	printf("\nTemp. in degree Fahrenheit is: %lf\n",f);
 
